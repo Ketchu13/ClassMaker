16e1fc60ee26fa72285d48dc2d932801a126f95b
Nicolas Grimaud <ketchu13@hotmail.com>
2015-10-22 06:02:49 +0200

    Add CSharp version

---
+++ src/ClassMaker/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ClassMaker_CSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClassMaker_CSharp")]
+[assembly: AssemblyCopyright("Copyright ©  ketchu13 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("83ae1b0f-fe0e-49c1-b4c4-70c7483fa536")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
